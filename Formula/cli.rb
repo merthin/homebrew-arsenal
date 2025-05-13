093f41da88e0d03ace83d64fb3e56998f122d969
Freddy Cubas <Hexagonalstar64@gmail.com>
2025-05-13 19:56:47 -0400

    Follow Homebrew style guidelines

--- Formula/cli.rb
+++ Formula/cli.rb
@@ -4,7 +4,7 @@
 #  https://docs.brew.sh/Formula-Cookbook
 #  https://rubydoc.brew.sh/Formula
 class Cli < Formula
-  desc "Arsenal Command Line Interface"
+  desc "Arsenal Command-line Interface"
   homepage "https://arsenal.merthin.com/"
   url "https://npm.merthin.net/@ced/cli/-/cli-1.1.79.tgz"
   sha256 "331cd64b2f2ead1840ad19c680961202af14da2a218d585933839dcf0626b404"
@@ -13,13 +13,12 @@ class Cli < Formula
   depends_on "node@22"
 
   def install
-    
     # Arsenal CLI depends on merthin's npm packages, which outside of the
     # offical npm registry. So we need to tell npm where to go look for them.
-    # Due to Homebrew's sandboxing, this configuration change won't affect 
+    # Due to Homebrew's sandboxing, this configuration change won't affect
     # other npm based Homebrew Formula.
 
-    npm_config_flags = ['-ddd', '--global', "--prefix=#{libexec}"]
+    npm_config_flags = ["-ddd", "--global", "--prefix=#{libexec}"]
     registry_flags = ["@ced:registry", "https://npm.merthin.net/"]
 
     system "npm", "config", "set", *registry_flags, *npm_config_flags
