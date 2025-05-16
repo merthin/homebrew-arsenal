2246d054cbdb2f77232db06c2ea8ccbdf01ffa00
ced-automation <>
2025-05-16 23:09:19 +0000

    Update to version 1.2.33

--- Formula/cli.rb
+++ Formula/cli.rb
@@ -6,15 +6,15 @@
 class Cli < Formula
   desc "Arsenal Command-line Interface"
   homepage "https://arsenal.merthin.com/"
-  url "https://npm.merthin.net/@ced/cli/-/cli-1.1.79.tgz"
-  sha256 "331cd64b2f2ead1840ad19c680961202af14da2a218d585933839dcf0626b404"
+  url "https://npm.merthin.net/@merthin/cli/-/cli-1.2.33.tgz"
+  sha256 "e7882edc8e941114d2a39d7554e962bbe0d9d5daeada8f1c6e3df21f9a5c2cc0"
   license "ISC"
 
   depends_on "node@22"
 
   def install
     # Arsenal CLI depends on merthin's npm packages, which outside of the
-    # offical npm registry. So we need to tell npm where to go look for them.
+    # official npm registry. So we need to tell npm where to go look for them.
     # Due to Homebrew's sandboxing, this configuration change won't affect
     # other npm based Homebrew Formula.
 
