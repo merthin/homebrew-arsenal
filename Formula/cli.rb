89ee9f5a6007f0467d3cecdaad2209a987233362
ced-automation <>
2025-05-21 00:06:34 +0000

    Update to version 1.1.39

--- Formula/cli.rb
+++ Formula/cli.rb
@@ -6,8 +6,8 @@
 class Cli < Formula
   desc "Arsenal Command-line Interface"
   homepage "https://arsenal.merthin.com/"
-  url "https://npm.merthin.net/@merthin/cli/-/cli-1.1.36.tgz"
-  sha256 "229f6a70ef5fbfd7ab2d4f94936152c377b9d4303391905a260ab857cc73573f"
+  url "https://npm.merthin.net/@merthin/cli/-/cli-1.1.39.tgz"
+  sha256 "e91971bae1a6f03a6cb3cdc453b9a636e4f19541d1eee29627e84c53059214af"
   license "ISC"
 
   depends_on "node@22"
